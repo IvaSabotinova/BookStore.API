05378c113b70117d16d3081c2a0b30d105803627
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-07-14 18:04:46 +0300

    First Commit

---
+++ BookStore.API/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.API.Models;
+using BookStore.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            List<BookModel> books = await bookRepository.GetAllBooksAsync();
+            return Ok(books);
+        }
+    }
+}
---
+++ BookStore.API/Repository/BookRepository.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.API.Data;
+using BookStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookStoreContext context;
+
+        public BookRepository(BookStoreContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<BookModel>> GetAllBooksAsync()
+        {
+            return await context.Books.Select(x => new BookModel
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description,
+            })
+           .ToListAsync();            
+        }     
+       
+        
+    }
+}
